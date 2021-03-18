6fffd78b19c661bd60fc104635c34670ea895015
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-03-18 23:53:56 +0200

    use eslint-barecheck-base-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-  env: {
-    node: true,
-    mocha: true
-  },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 11
-  },
-  rules: {
-    'prettier/prettier': 'error'
-  }
+  extends: ['eslint-config-barecheck-base'],
+  rules: {}
 };
