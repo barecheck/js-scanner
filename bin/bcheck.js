5cb462c1d4c248fec7bac9233b0f0c2656d486b1
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-07-07 23:30:15 +0100

    feat: add commander library and init checks command (#16)

--- bin/bcheck.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const { checkDuplicates } = require('../src/checks/duplicates');
-
-async function main() {
-  await checkDuplicates();
-}
-
-main();
---
+++ src/commands/duplication/check.js
@@ -0,0 +1,6 @@
+const duplicationChecksCommand = (path) => {
+  // eslint-disable-next-line no-console
+  console.log(`Running checks over ${path} directory`);
+};
+
+module.exports = duplicationChecksCommand;
